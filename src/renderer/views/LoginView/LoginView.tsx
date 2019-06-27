ba4c28de6467a99fcea0c0b23714a31b283ea7d6
varHarrie <varharrie@gmail.com>
2019-06-27 19:42:23 +0800

    Remove redundant space

--- src/renderer/views/LoginView/LoginView.tsx
+++ src/renderer/views/LoginView/LoginView.tsx
@@ -30,7 +30,7 @@ export default observer(function LoginView (props: Props) {
       try {
         await Database.connect('./store.passhub', password)
       } catch (error) {
-        message('XCircle', 'Incorrect password !')
+        message('XCircle', 'Incorrect password!')
         return
       }
 
